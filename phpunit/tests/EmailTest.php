10f4bdf5c8ea488d53e73a275167b6020d0e2f12
=Humbal Shahi <=sh.humble@gmail.com>
2021-01-10 00:19:16 -0800

    Added Annotation

---
+++ phpunit/tests/EmailTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class EmailTest extends TestCase
+{
+    /**
+     * @dataProvider emailsProvider
+     */
+    public function testValidEmail($email) {
+        $this->assertRegExp('/^.+\@\S+\.\S+$/', $email);
+    }
+
+    public function emailsProvider(){
+        return[
+            ['abc@def.com'],
+            ['xyz@pqrs.com'],
+            ['ghik12@klosp.com'],
+        ];
+    }
+
+    /**
+     * @dataProvider numbersProvider
+     */
+    public function testMath($a, $b, $expected) {
+        $result = $a * $b;
+        return $this->assertEquals($expected, $result);
+    }
+
+    public function numbersProvider() {
+        return [
+            [1,2,2],
+            [2,2,4],
+            [3,3,9],
+        ];
+
+    }
+    
+}
